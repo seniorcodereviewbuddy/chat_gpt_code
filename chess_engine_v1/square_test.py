15e9c1986442d86d412bc1b387db13f40716fa0c
Chris Sharp <chris@seniorcodereviewbuddy.com>
2024-07-19 12:15:30 -0400

    Enable strict mypy typing for move_test and square_test
    Fix all related errors, which are jsut adding simple "-> None"
    to the tests.

--- chess_engine_v1/square_test.py
+++ chess_engine_v1/square_test.py
@@ -4,13 +4,13 @@ from square import Square, Squares
 
 
 class TestSquare(unittest.TestCase):
-    def test_square_initialization(self):
+    def test_square_initialization(self) -> None:
         square = Square("e4")
         self.assertEqual(square.algebraic, "e4")
         self.assertEqual(square.row, 3)
         self.assertEqual(square.col, 4)
 
-    def test_square_row(self):
+    def test_square_row(self) -> None:
         self.assertEqual(Squares.A1.row, 0)
         self.assertEqual(Squares.B8.row, 7)
         self.assertEqual(Squares.C2.row, 1)
@@ -20,7 +20,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(Squares.G4.row, 3)
         self.assertEqual(Squares.H5.row, 4)
 
-    def test_square_col(self):
+    def test_square_col(self) -> None:
         self.assertEqual(Squares.A1.col, 0)
         self.assertEqual(Squares.B8.col, 1)
         self.assertEqual(Squares.C2.col, 2)
@@ -30,31 +30,31 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(Squares.G4.col, 6)
         self.assertEqual(Squares.H5.col, 7)
 
-    def test_square_str(self):
+    def test_square_str(self) -> None:
         square = Square("e4")
         self.assertEqual(str(square), "e4")
 
-    def test_square_repr(self):
+    def test_square_repr(self) -> None:
         square = Square("e4")
         self.assertEqual(repr(square), "e4")
 
-    def test_square_equality(self):
+    def test_square_equality(self) -> None:
         square1 = Square("e4")
         square2 = Square("e4")
         self.assertEqual(square1, square2)
 
-    def test_square_inequality(self):
+    def test_square_inequality(self) -> None:
         square1 = Square("e4")
         square2 = Square("d4")
         self.assertNotEqual(square1, square2)
 
-    def test_square_hash(self):
+    def test_square_hash(self) -> None:
         square1 = Square("e4")
         square2 = Square("e4")
         square_set = {square1, square2}
         self.assertEqual(len(square_set), 1)
 
-    def test_square_from_row_col(self):
+    def test_square_from_row_col(self) -> None:
         self.assertEqual(Squares.square_from_row_col(0, 0), Squares.A1)
         self.assertEqual(Squares.square_from_row_col(7, 7), Squares.H8)
         self.assertEqual(Squares.square_from_row_col(4, 4), Squares.E5)
