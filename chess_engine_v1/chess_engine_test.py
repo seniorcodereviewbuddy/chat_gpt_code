bc2271854dd3775b9d576b913650b895a987bf11
Chris <chris@seniorcodereviewbuddy.com>
2024-08-08 16:59:27 -0400

    Fix chess_engine_test tests (#31)
    Remove SetUp since it was also hiding some important test details.

--- chess_engine_v1/chess_engine_test.py
+++ chess_engine_v1/chess_engine_test.py
@@ -6,35 +6,29 @@ from chess_engine import ChessEngine, uci_algebraic_notation
 from move import Move
 from square import Squares
 
-# TODO: Chris: Fix all tests.
-
 
 class TestChessEngine(unittest.TestCase):
-    def setUp(self) -> None:
-        # Initialize the board and engine with a starting position
-        self.board = Board(fen.STARTING_GAME_FEN)
-        self.engine = ChessEngine(self.board)
-
     def test_evaluate(self) -> None:
+        board = Board(fen.STARTING_GAME_FEN)
+        engine = ChessEngine(board)
         # Test the evaluation function
-        self.assertEqual(self.engine.evaluate(), 0)  # Initial position is balanced
+        self.assertEqual(engine.evaluate(), 0)  # Initial position is balanced
 
         # Test a board with a material imbalance
-        self.board.board[4][4] = "Q"  # Add a white queen in the center
-        self.assertEqual(self.engine.evaluate(), 9)
+        board.board[4][4] = "Q"  # Add a white queen in the center
+        self.assertEqual(engine.evaluate(), 9)
 
-    @unittest.expectedFailure
     def test_best_move(self) -> None:
-        # White has 2 pawn that can't move and a king in the corner that can only move
+        # White has a pawn that can't move and a king in the corner that can only move
         # to one square. So there is only one move white can make.
-        fen_str = "8/8/8/8/7k/6pp/6PP/7K w - - 0 1"
-        board = Board(fen_str)  # noqa
-        engine = ChessEngine(self.board)  # noqa
+        fen_str = "8/8/8/8/7k/7p/7P/7K w - - 0 1"
+        board = Board(fen_str)
+        engine = ChessEngine(board)
 
         # Test the best move function
-        move = self.engine.best_move(1)
-        # TODO: Chris: Pretty sure this is wrong, but it's at least constant.
-        self.assertEqual(move, ((7, 3), (1, 3)))
+        move = engine.best_move(1)
+        expected_move = Move(Squares.H1, Squares.G1, "K")
+        self.assertEqual(move, expected_move)
 
 
 class TestUCIAlgebraicNotation(unittest.TestCase):
